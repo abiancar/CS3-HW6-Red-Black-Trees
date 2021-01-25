c3bfcb393a824bda7b596077e0c876734caa5ef9
yunjic97 <76976583+yunjic97@users.noreply.github.com>
2021-01-25 17:03:40 +0000

    Add files via upload

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -1,6 +1,82 @@
 #include <iostream>
 #include <string>
 #include "RedBlackTree.h"
+#include <assert.h>
 
-RBTree::RBTree(){     
+using namespace std; 
+
+// constructor 
+RedBlackTree::RedBlackTree()
+{   
+	// no action needed 
 }
+RedBlackTree::~RedBlackTree()
+{}
+
+void RedBlackTree::Insert(int x)
+{
+	// if the size of the tree is 0
+	if (numItems == 0)
+	// create a node, make the node the root node 
+	{
+		RBTNode* rbt = new RBTNode(); 
+		rbt->data = x; 
+		this->root = rbt; 
+	}
+	
+}
+
+bool RedBlackTree::Contains(int)
+{
+	return true; 
+}
+
+int RedBlackTree::GetMin()
+{
+	return 0; 
+}
+
+int RedBlackTree::GetMax()
+{
+	return 0; 
+}
+
+int RedBlackTree::Size()
+{
+	return 0; 
+}
+
+/*
+string RedBlackTree::ToInfixString() const
+{
+	string s = "";
+	return s; 
+}
+string RedBlackTree::ToPrefixString() const 
+{
+	string s = "";
+	return s; 
+}
+string RedBlackTree::ToPostfixString() const 
+{
+	string s = "";
+	return s; 
+}
+
+string InfixString(RBTNode*)
+{
+	string s = "";
+	return s; 
+}
+
+string PrefixString(RBTNode*)
+{
+	string s = "";
+	return s; 
+}
+
+string PostfixString(RBTNode*)
+{
+	string s = "";
+	return s; 
+}*/
