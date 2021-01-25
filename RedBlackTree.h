38b859e7133d40906c837498718de428f7b25712
Agustin Biancardi <agus@pop-os.localdomain>
2021-01-25 09:54:46 -0500

    Second commit, created usable RedBlackTree.h file

--- RedBlackTree.h
+++ RedBlackTree.h
@@ -1,15 +1,34 @@
-struct Node {
-	char key;
-	int data; // Not necessary or used for Dijkstra algorithm
+#include <string>
+using namespace std;
+struct RBTNode {
+	int data;
+    unsigned short int color;
+    RBTNode *left = nullptr;
+    RBTNode *right = nullptr; 
 };
-
 
 class RBTree{
 	
 	public:
 		RBTree();
         ~RBTree();
+        
+        void Insert(int);
+        bool Contains(int);
+        
+        int GetMin();
+        int GetMax();
+        int Size();
+        
+        std::string ToInfixString() const {return InfixString(root);};
+        std::string ToPrefixString() const {return PrefixString(root);};
+        std::string ToPostfixString() const {return PostfixString(root);};
 	private:
-    
+        RBTNode *root;
+        unsigned long long int numItems;
+
+        static std::string InfixString(RBTNode*);
+        static std::string PrefixString(RBTNode*); 
+        static std::string PostfixString(RBTNode*); 
 
 };
